e8f00e0379b0d3d98398492b5132842ab411ae18
Francisco Calaça <chicocx@gmail.com>
2016-08-24 11:54:30 -0300

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -2,28 +2,30 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
 
-
 public class Player : NetworkBehaviour {
 
+
     public int moveX = 0;
+
     public int moveY = 0;
+
     public float moveSpeed = 0.2f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveX * moveSpeed, moveY * moveSpeed, 0);
+
         if (!isLocalPlayer)
         {
             return;
         }
-
-        // input handling for local player only
-        int oldMoveX = moveX;
-        int oldMoveY = moveY;
 
         moveX = 0;
         moveY = 0;
@@ -44,27 +46,12 @@ public class Player : NetworkBehaviour {
         {
             moveY -= 1;
         }
-        if (moveX != oldMoveX || moveY != oldMoveY)
-        {
-            CmdMove(moveX, moveY);
-        }
-	
-	}
 
-	[Command]
-    public void CmdMove(int x, int y)
-    {
-        moveX = x;
-        moveY = y;
-        transform.Translate(moveX * moveSpeed, moveY * moveSpeed, 0);
-        base.SetDirtyBit(1);
+
     }
-	
-    [ServerCallback]
+
     public void FixedUpdate()
     {
         transform.Translate(moveX * moveSpeed, moveY * moveSpeed, 0);
     }
-	
-	
 }
